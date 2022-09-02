dc9c1db9d7d0fc9d2d6d078220ccfb1aae09888d
Felipe Fernandes <felipedfe@gmail.com>
2022-09-02 13:29:17 -0300

    teste

--- src/pages/StartScreen.js
+++ src/pages/StartScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import myContext from '../context/myContext';
 import { backImage1, backImage2, backImage3 } from '../images/back-image';
-import '../style/startscreen.css'
+import '../style/StartScreen.css'
 
 function StartScreen({ children }) {
   // State
